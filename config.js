ae39071483c351888e3e04698530d8e86f46270e
Corentin Crébier <crebier.corentin@gmail.com>
2020-12-10 17:57:31 +0100

    Setup database schema

--- config.js
+++ config.js
@@ -1,7 +1,15 @@
-require('dotenv').config();
+const path = require('path');
+
+require('dotenv').config({ path: path.join(__dirname, './.env') });
 
 // Feel free to add your own settings,
 // e.g. DB connection settings
 module.exports = {
   port: process.env.PORT || 5000,
+  db: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
 };
---
+++ db/knexfile.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const config = require('../config');
+
+module.exports = {
+  client: 'mysql2',
+  connection: {
+    host: config.db.host,
+    user: config.db.user,
+    password: config.db.password,
+    database: config.db.database,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: path.join(__dirname, './migrations/'),
+  },
+};
