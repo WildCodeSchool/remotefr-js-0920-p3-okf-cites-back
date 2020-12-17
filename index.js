d62bb8422f08802577c402af070a64340650ac20
Corentin Crébier <crebier.corentin@gmail.com>
2020-12-17 11:48:46 +0100

    Add compression and logging middlewares

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
+const morgan = require('morgan');
 const { port } = require('./config');
 
 const explorerRouter = require('./routes/explore.js');
@@ -8,8 +10,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-app.get('/', (req, res) => res.send('Express server is up and running!'));
+app.use(compression());
+app.use(morgan('dev'));
 
 app.use('/explore', explorerRouter);
 
