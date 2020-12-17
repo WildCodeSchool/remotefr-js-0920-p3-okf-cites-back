10d4403cc51e255fe75bb0fa87ec7ba398684e93
Corentin Crébier <crebier.corentin@gmail.com>
2020-12-17 21:38:23 +0100

    Add filters to search

--- routes/explore.js
+++ routes/explore.js
@@ -4,14 +4,35 @@ const knex = require('../db/knex');
 const router = express.Router();
 
 router.get('/search', async (req, res) => {
-  const movieSearch = req.query.search;
+  const { query, cites, animal } = req.query;
+  const plant = req.query.plant === 'true';
 
-  const result = await knex('species')
+  const qb = knex('species')
     .select('*')
-    .where('name', 'like', `%${movieSearch}%`)
-    .orWhere('common_name', 'like', `%${movieSearch}%`)
-    .limit(20);
-  res.json(result);
+    .limit(20)
+    .where((builder) =>
+      builder
+        .where('name', 'like', `%${query}%`)
+        .orWhere('common_name', 'like', `%${query}%`),
+    )
+    .where((builder) => {
+      if (animal?.length > 0) {
+        builder.whereIn('class', animal);
+      }
+
+      if (plant) {
+        builder.orWhere('kingdom', '=', 'Plantae');
+      }
+    });
+
+  if (cites?.length > 0) {
+    // Add I/II to where if it contains I or II
+    if (cites.some((c) => c === 'I' || c === 'II')) cites.push('I/II');
+
+    qb.whereIn('cites', cites);
+  }
+
+  res.json(await qb);
 });
 
 module.exports = router;
