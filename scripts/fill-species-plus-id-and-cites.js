57e53fdb261aed890eeeac0ec1594ebee0ae1d49
Corentin Crébier <crebier.corentin@gmail.com>
2021-01-28 11:26:01 +0100

    Fill cites appendix from species+ dataset

--- scripts/fill-species-plus-id-and-cites.js
+++ scripts/fill-species-plus-id-and-cites.js
@@ -5,7 +5,7 @@ const knex = require('../db/knex');
 const { Semaphore } = require('../utils');
 
 async function main() {
-  const speciesPlus = JSON.parse(
+  const speciesPlusData = JSON.parse(
     fs.readFileSync(
       path.join(
         __dirname,
@@ -13,32 +13,36 @@ async function main() {
       ),
     ),
   );
-  const speciesPlusIdByName = speciesPlus.reduce((obj, species) => {
-    obj[species['Scientific Name'].toLowerCase()] = species.Id;
+  const speciesPlusByName = speciesPlusData.reduce((obj, species) => {
+    obj[species['Scientific Name'].toLowerCase()] = {
+      id: species.Id,
+      cites: species.Listing,
+    };
     return obj;
   }, {});
 
   const speciesWithMissingSpeciesPlusId = await knex('species')
     .whereNull('species+_id')
-    .select('id', 'name', 'subspecies');
+    .orWhere('cites', '=', '?')
+    .select('id', 'name');
 
   const semaphore = new Semaphore(20);
 
   await Promise.all(
     speciesWithMissingSpeciesPlusId.map(async (species) => {
-      let name = species.name.toLowerCase();
-      if (species.subspecies != null) {
-        name += ` ${species.subspecies}`.toLowerCase();
-      }
+      const name = species.name.toLowerCase();
 
-      const speciesPlusId = speciesPlusIdByName[name];
-      if (speciesPlusId != null) {
+      const speciesPlus = speciesPlusByName[name];
+      if (speciesPlus != null) {
         await semaphore.acquire();
         try {
           console.log(`Found species+_id for ${name}`);
 
           await knex('species')
-            .update({ 'species+_id': speciesPlusId })
+            .update({
+              'species+_id': speciesPlus.id,
+              cites: speciesPlus.cites,
+            })
             .where('id', '=', species.id);
         } finally {
           await semaphore.release();
